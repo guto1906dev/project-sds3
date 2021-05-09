bc5a17b4b7a4204f3d691d6bee6faabb596dd098
guto1906dev <guto1906@yahoo.com.br>
2021-05-08 23:58:25 -0300

    Layers

---
+++ backend/src/main/java/com/guto1906dev/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.guto1906dev.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guto1906dev.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
