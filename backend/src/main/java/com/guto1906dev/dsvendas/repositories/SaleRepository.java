f5c559d312e905cf445008db3828208812bc7ce2
guto1906dev <guto1906@yahoo.com.br>
2021-05-09 00:43:24 -0300

    Pagination

---
+++ backend/src/main/java/com/guto1906dev/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.guto1906dev.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guto1906dev.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
