f21f9d9af297fd7cff6a896f5cb72d66c0dfafba
guto1906dev <guto1906@yahoo.com.br>
2021-05-22 00:28:02 -0300

    PUT /{id}/deliveredv - SDS2

--- backend/src/main/java/com/guto1906dev/dsvendas/services/OrderService.java
+++ backend/src/main/java/com/guto1906dev/dsvendas/services/OrderService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.datetime.standard.InstantFormatter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,38 +12,45 @@ import com.guto1906dev.dsvendas.dto.OrderDTO;
 import com.guto1906dev.dsvendas.dto.ProductDTO;
 import com.guto1906dev.dsvendas.entities.Order;
 import com.guto1906dev.dsvendas.entities.OrderStatus;
-import com.guto1906dev.dsvendas.entities.Product;
 import com.guto1906dev.dsvendas.repositories.OrderRepository;
 import com.guto1906dev.dsvendas.repositories.ProductRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository repository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
+	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrderWithProducts();
-		
-		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());		
-		
+
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
 	}
-	
+
 	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),
-				dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
-		for(ProductDTO productDTO : dto.getProducts() ) {
-			order.getProducts().add(productRepository.getOne(productDTO.getId()));			
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO productDTO : dto.getProducts()) {
+			order.getProducts().add(productRepository.getOne(productDTO.getId()));
 		}
 		order = repository.save(order);
-			
+
 		return new OrderDTO(order);
-		
+
+	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
